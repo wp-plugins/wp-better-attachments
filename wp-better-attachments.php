a7ac4d867d05afbc3e07f2cb750c79ad765fef57
dholloran <dholloran@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-29 20:57:44 +0000

    Setup better ignores
    git-svn-id: https://plugins.svn.wordpress.org/wp-better-attachments/trunk@780013 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-better-attachments.php
+++ wp-better-attachments.php
@@ -1,56 +1,63 @@
 <?php
 /**
-* @package WP_Better_Attachments
-* @version 1.3.7
-*/
+ *
+ *
+ * @package WP_Better_Attachments
+ * @version 1.3.8
+ */
 /*
 Plugin Name: WP Better Attachments
 Plugin URI: http://dholloran.github.io/wp-better-attachments
 Description: Better Wordpress Attachments
 Author: Dan Holloran
-Version: 1.3.7
+Version: 1.3.8
 Author URI: http://danholloran.com/
 */
 
-define( 'WPBA_VERSION', '1.3.7' );
+define( 'WPBA_VERSION', '1.3.8' );
 define( 'WPBA_LANG', 'wpba' );
-define( 'WPBA_PATH', plugin_dir_path(__FILE__) );
+define( 'WPBA_PATH', plugin_dir_path( __FILE__ ) );
 
 /*
 * Pretty Print Debug Function
 *
 * Only on localhost
 */
-if ( !function_exists( 'pp' ) AND $_SERVER['HTTP_HOST'] == 'localhost' ) {
-	function pp( $value )
-	{
-		if( $_SERVER['HTTP_HOST'] != 'localhost' ) return;
-		echo "<pre>";
+if ( !function_exists( 'pp' ) and $_SERVER['HTTP_HOST'] == 'localhost' ) {
+	function pp( $value ) {
+		if ( $_SERVER['HTTP_HOST'] != 'localhost' ) return;
+		echo '<pre>';
 		if ( $value ) {
 			print_r( $value );
 		} else {
 			var_dump( $value );
 		}
-		echo "</pre>";
+		echo '</pre>';
 	} // pp()
 } // if()
 
 
 /**
-* Required Classes
-*/
-require_once "libs/wp-settings-api-bootstrap/class.wp-settings-api-bootstrap.php";
-require_once "classes/class-wp-better-attachments.php";
-require_once "classes/class-wpba-media-library.php";
-require_once "classes/class-wpba-meta-box.php";
-require_once "classes/class-wpba-crop-resize.php";
-require_once "classes/class-wpba-ajax.php";
-require_once "classes/class-wpba-settings.php";
-require_once "classes/class-wpba-settings-fields.php";
-require_once "classes/class-wpba-frontend.php";
+ * Composer AutoLoad
+ */
+require_once 'vendor/autoload.php';
 
 
 /**
-* Includes
-*/
-require_once "inc/shortcodes.inc.php";
+ * Required Classes
+ */
+require_once 'libs/wp-settings-api-bootstrap/class.wp-settings-api-bootstrap.php';
+require_once 'classes/class-wp-better-attachments.php';
+require_once 'classes/class-wpba-media-library.php';
+require_once 'classes/class-wpba-meta-box.php';
+require_once 'classes/class-wpba-crop-resize.php';
+require_once 'classes/class-wpba-ajax.php';
+require_once 'classes/class-wpba-settings.php';
+require_once 'classes/class-wpba-settings-fields.php';
+require_once 'classes/class-wpba-frontend.php';
+
+
+/**
+ * Includes
+ */
+require_once 'inc/shortcodes.inc.php';
