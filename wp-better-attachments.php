82f23e915795777886f247776e1e6cf0c175bda9
dholloran <dholloran@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-30 02:56:13 +0000

    Changed version number
    git-svn-id: https://plugins.svn.wordpress.org/wp-better-attachments/trunk@780098 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-better-attachments.php
+++ wp-better-attachments.php
@@ -3,18 +3,18 @@
  *
  *
  * @package WP_Better_Attachments
- * @version 1.3.8
+ * @version 1.3.7
  */
 /*
 Plugin Name: WP Better Attachments
 Plugin URI: http://dholloran.github.io/wp-better-attachments
 Description: Better Wordpress Attachments
 Author: Dan Holloran
-Version: 1.3.8
+Version: 1.3.7
 Author URI: http://danholloran.com/
 */
 
-define( 'WPBA_VERSION', '1.3.8' );
+define( 'WPBA_VERSION', '1.3.7' );
 define( 'WPBA_LANG', 'wpba' );
 define( 'WPBA_PATH', plugin_dir_path( __FILE__ ) );
 
